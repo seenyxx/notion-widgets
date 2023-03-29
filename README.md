07d9207f278f1e8f63982a67982023659a89a758
seen-idc <seen.idc@gmail.com>
2023-03-29 18:50:12 +1100

    made title widget

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Notion Widgets
-- Title
+### Title Widget
+Url: /notion-widgets/title-widget
+Parameters:
+- `?text` Text to be displayed
+- `?size` Size of the text in pixels
+- `?font` can be `default`, `serif`, `mono` and `sc` (small caps)
+- `?dark` Enter any value to change the background to fit dark mode
--- title-widget/index.js
+++ title-widget/index.js
@@ -1,8 +1,19 @@
+const darkModeBg = '#191919';
+const lightModeBg = '#ffffff';
+
+
 const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
 });
 
-let titleElement = document.getElementById('title-text')
+let titleElement = document.getElementById('title-text');
 
-titleElement.innerText = params.text;
+// Use query ?text to change the title text
+if (params.text)
+  titleElement.innerText = params.text;
 
+titleElement.className = params.font ? `font-${params.font}` : 'font-default';
+titleElement.style.fontSize = params.size ? `${params.size}px` : '72px';
+
+document.body.style.backgroundColor = params.dark ? darkModeBg : lightModeBg;
+titleElement.style.color = params.dark ? 'white' : 'black';
